914c3fa9249df4ab4f73e7cbe6dea8c5ebe0bea7
omermizrahi1 <60889997+omermizrahi1@users.noreply.github.com>
2021-12-01 20:33:13 +0200

    ok

---
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -0,0 +1,38 @@
+package bgu.spl.mics;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FutureTest {
+
+    static Future future;
+
+    @Before
+    public void setUp() throws Exception {
+        future = new Future();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        future=null;
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void resolve() {
+    }
+
+    @Test
+    public void isDone() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+}
